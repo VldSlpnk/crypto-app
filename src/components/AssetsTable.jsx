f5d432d7dc14ef328043fa4b38933141fce8e3d7
Vlad Slipenkyi <defendervdele@gmail.com>
2024-04-26 11:26:04 +0300

    first

---
+++ src/components/AssetsTable.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Table } from 'antd'
+import { useCrypto } from '../context/crypto-context'
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    showSorterTooltip: {
+      target: 'full-header',
+    },
+
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ['descend'],
+  },
+  {
+    title: 'Price, $',
+    dataIndex: 'price',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.price - b.price,
+    sortDirections: ['descend'],
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.amount - b.amount,
+    sortDirections: ['descend'],
+  },
+]
+
+const AssetsTable = () => {
+  const { assets } = useCrypto()
+  const data = assets.map((a) => ({
+    key: a.id,
+    name: a.name,
+    price: a.price,
+    amount: a.amount,
+  }))
+
+  return <Table pagination={false} columns={columns} dataSource={data} />
+}
+
+export default AssetsTable
